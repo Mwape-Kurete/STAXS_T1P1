5f1500b2f9ea508bcada7665564aee15a598930d
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-02-28 17:49:36 +0200

    bootstrap
    bootstrap installed

---
+++ newapp_bs5/src/Sidebar.js
@@ -0,0 +1,13 @@
+import './Sidebar.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Sidebar() {
+    return (
+        <div className="container-fluid">
+
+        </div>
+    );
+
+}
+
+export default Sidebar; 
