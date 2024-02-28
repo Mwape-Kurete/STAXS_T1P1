1e516ce502cf2f7eb8b5bdcbfa489df776a15422
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-02-28 19:10:41 +0200

    added bootstrap begun coding sidebar

--- newapp_bs5/src/Sidebar.js
+++ newapp_bs5/src/Sidebar.js
@@ -1,13 +1,32 @@
-import './Sidebar.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./Sidebar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function Sidebar() {
-    return (
-        <div className="container-fluid">
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="bg-dark col-auto col-md-3 min-vh-100">
+          <a className="text-decoration-none text-white d-flex align-itemcenter">
+            <i className="fs-4 bi bi-speedometer"></i>
+            <span className="ms-1 fs-4">Brand</span>
+          </a>
 
+          <ul class="nav">
+            <li class="nav-item">
+              <a href="" class="nav-link"></a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link"></a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link"></a>
+            </li>
+          </ul>
         </div>
-    );
-
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar; 
+export default Sidebar;
