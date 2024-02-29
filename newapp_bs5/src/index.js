3628994b88b7bd5f905436028f3ba77c1d000151
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-02-29 10:54:49 +0200

    testing cards

--- newapp_bs5/src/index.js
+++ newapp_bs5/src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Sidebar from "./Sidebar.js";
+import Cards from "./Cards.js";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <Sidebar />
-  </React.StrictMode>
+    <Cards />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
