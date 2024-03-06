8bc01c0d1a671b7602243f193bba7eca96f2e2d3
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-03-06 20:46:57 +0200

    fixing routing

--- newapp_bs5/src/Timeline.js
+++ newapp_bs5/src/Timeline.js
@@ -2,9 +2,15 @@ import Sidebar from "./Sidebar";
 
 function Timeline() {
   return (
-    <div>
-      <Sidebar />
-      <h1>No code here yet</h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-2 sidebar-col">
+          <Sidebar />
+        </div>
+        <div className="col justify-content-md-center">
+          <h1>No code here yet</h1>
+        </div>
+      </div>
     </div>
   );
 }
