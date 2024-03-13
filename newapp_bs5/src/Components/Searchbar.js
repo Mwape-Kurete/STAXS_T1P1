2e5933099b848e03614d69b70adb62155b678aec
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-03-13 21:15:37 +0200

    styling time line

--- newapp_bs5/src/Components/Searchbar.js
+++ newapp_bs5/src/Components/Searchbar.js
@@ -9,7 +9,7 @@ function Searchbar() {
             <input
               className="form-control me-2"
               type="search"
-              placeholder="Search For Artists to Compare..."
+              placeholder="Search..."
               aria-label="Search"
             />
             <button className="btn btn-outline-success" type="submit">
--- newapp_bs5/src/Pages/Timeline.js
+++ newapp_bs5/src/Pages/Timeline.js
@@ -1,4 +1,6 @@
 import Sidebar from "../Components/Sidebar";
+import Searchbar from "../Components/Searchbar";
+import Solange from "../img/Solange - A Seat At the Table (2 LPs) - 2 Vinyl LP _ 08-12-2016 _ Regulier Vinyl.jpeg";
 import "../Styles/Timeline.css";
 
 function Timeline() {
@@ -10,27 +12,44 @@ function Timeline() {
         </div>
         <div className="col justify-content-md-center">
           <div className="row">
+            <div className="col-12 searchbar">
+              <Searchbar />
+            </div>
+            {/*album timeline goes below*/}
             <div className="col-12 albmun-timeline-cont">
-              <div className="album-info">
-                <img className="album-cover" id="albumCover" src="" alt="" />
-                <h1 id="album-name">A Seat at the table</h1>
-                <h3 id="album-year">
-                  Album Release year{" "}
-                  <span className="album-meta" id="yearMeta">
-                    2017
-                  </span>
-                </h3>
-                <h3 id="album-tracks">
-                  Number of tracks
-                  <span className="album-meta" id="trackMeta">
-                    2017
-                  </span>
-                </h3>
+              <div className="album-info justify-content-md-center">
+                <img
+                  className="album-cover"
+                  id="albumCover"
+                  src={Solange}
+                  alt=""
+                />
+                <div className="all_album_info">
+                  <h6 id="album-name">A Seat at the table</h6>
+                  <p id="album-year">
+                    Album Release year:{" "}
+                    <span className="album-meta" id="yearMeta">
+                      2017
+                    </span>
+                  </p>
+                  <p id="album-tracks">
+                    Number of tracks:{" "}
+                    <span className="album-meta" id="trackMeta">
+                      2017
+                    </span>
+                  </p>
+                </div>
               </div>
             </div>
             <div className="col-12 timeline-cont">
-              <div className="filters"></div>
-              <p>timelines Go here</p>
+              <div className="row">
+                <div className="col timeline">
+                  <p>timelines Go here</p>
+                </div>
+                <div className="col-2 filters">
+                  <p>filters go here</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
