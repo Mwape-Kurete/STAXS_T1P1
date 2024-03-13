d141b610f873fbcf39f7f2a3bbab0b53027e6d4d
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-03-13 16:46:55 +0200

    coding timeline

--- newapp_bs5/src/Pages/Timeline.js
+++ newapp_bs5/src/Pages/Timeline.js
@@ -9,7 +9,30 @@ function Timeline() {
           <Sidebar />
         </div>
         <div className="col justify-content-md-center">
-          <h1>No code here yet</h1>
+          <div className="row">
+            <div className="col-12">
+              <div className="album-info">
+                <img className="album-cover" id="albumCover" src="" alt="" />
+                <h1 id="album-name">A Seat at the table</h1>
+                <h3 id="album-year">
+                  Album Release year{" "}
+                  <span className="album-meta" id="yearMeta">
+                    2017
+                  </span>
+                </h3>
+                <h3 id="album-tracks">
+                  Number of tracks
+                  <span className="album-meta" id="trackMeta">
+                    2017
+                  </span>
+                </h3>
+              </div>
+            </div>
+            <div className="col-12 timeline-cont">
+              <div className="filters"></div>
+              <p>timelines Go here</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
