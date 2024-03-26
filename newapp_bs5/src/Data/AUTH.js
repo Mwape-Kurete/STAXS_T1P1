0ba408d4854c2fb0e83c5464db7fa70ee012ed81
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-03-26 14:40:13 +0200

    update

--- newapp_bs5/src/Data/AUTH.js
+++ newapp_bs5/src/Data/AUTH.js
@@ -1,16 +1,14 @@
-import axios from "axios";
+// import axios from "axios";
 
-var client_id = "6dfe161492d14a558bea14512386b896";
-var client_secret = "7d449a9084ca46b2a30397b3d9ad11c8";
+// var client_id = "6dfe161492d14a558bea14512386b896";
+// var client_secret = "7d449a9084ca46b2a30397b3d9ad11c8";
 
+// async function AUTH() {
+// try{
+//   const response = await axios.get(
 
-async function AUTH() {
-try{
-  const response = await axios.get(
-    
+//   )
+// }
+// }
 
-  )
-}
-}
-
-export default AUTH;
+// export default AUTH;
