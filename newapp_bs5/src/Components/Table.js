95ab8d6d77e2a5cc03f3fd529a3b0ad435bfdb24
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-04-05 14:00:47 +0200

    making a table for the home page

---
+++ newapp_bs5/src/Components/Table.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import qs from "qs";
+
+import "../Styles/Table.css";
+
+function Table() {}
+
+export default Table;
