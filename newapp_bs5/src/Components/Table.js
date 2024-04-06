4c66d33cd1c2912f4f87f54d97bde27a48857d42
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-04-06 14:19:49 +0200

    changing API

--- newapp_bs5/src/Components/Table.js
+++ newapp_bs5/src/Components/Table.js
@@ -18,11 +18,11 @@ function Table() {
     const fetchBillboard = async () => {
       const options = {
         method: "GET",
-        url: "https://billboard-api2.p.rapidapi.com/hot-100",
-        params: { date: formattedDate, range: "1-10" },
+        url: "https://billboard-api5.p.rapidapi.com/api/charts/hot-100",
+        params: { date: formattedDate },
         headers: {
           "X-RapidAPI-Key": API_KEY,
-          "X-RapidAPI-Host": "billboard-api2.p.rapidapi.com",
+          "X-RapidAPI-Host": "billboard-api5.p.rapidapi.com",
         },
       };
 
@@ -30,45 +30,50 @@ function Table() {
         const response = await axios.request(options);
         console.log(response.data);
         // Adjust this line based on the actual structure of your API response
-        console.log(response.data.content);
-        setRankings(response.data.content || []);
-
-        console.log(rankings);
+        console.log(response.data.chart.entries);
+        setRankings(response.data.chart.entriest || []);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchBillboard();
+    console.log(rankings);
   }, []);
 
   return (
     <div className="mainTableCont">
       <div className="table-responsive">
         <h1>The Billboard Hot100 on {formattedDate}</h1>
-        <table className="table table-striped">
-          <thead>
+        <table className="table">
+          <thead className="tableHeader">
             <tr>
-              <th scope="col">Rank</th>
+              <th scope="col" id="tableImg">
+                Cover
+              </th>
+              <th scope="col">Ranking</th>
               <th scope="col">Title</th>
               <th scope="col">Artist</th>
-              <th scope="col">Last week</th>
               <th scope="col">Peak Position</th>
               <th scope="col">Weeks on Chart</th>
             </tr>
           </thead>
           <tbody>
-            {Array.isArray(rankings) &&
-              rankings.map((song, index) => (
-                <tr key={index}>
-                  <td>{song.rank}</td>
-                  <td>{song.title}</td>
-                  <td>{song.artist}</td>
-                  <td>{song.last_week}</td>
-                  <td>{song.peak_position}</td>
-                  <td>{song.weeks_on_chart}</td>
-                </tr>
-              ))}
+            {rankings.slice(0, 10).map((song) => (
+              <tr key={song.rank}>
+                <td>
+                  <img
+                    src={song.cover}
+                    alt="Cover"
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                </td>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.position.peakPosition}</td>
+                <td>{song.position.weeksOnChart}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
