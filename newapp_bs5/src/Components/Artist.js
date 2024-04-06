9141847e2d35a25075972a2abecdf9a00f57384b
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-04-06 23:14:36 +0200

    adding artists to Artist.js

--- newapp_bs5/src/Components/Artist.js
+++ newapp_bs5/src/Components/Artist.js
@@ -1,51 +1,60 @@
+import React, { useState, useEffect } from "react";
 import "../Styles/Artists.css";
 import Temp1 from "../img/6838e615-ab19-49fe-89c8-efbe6964f2bf.jpeg";
-
 import { CloseButton } from "react-bootstrap";
 
 function Artist() {
+  const [items, setItems] = useState([]);
+
+  //the following code ensures that my search bar is appropriatley updated
+
+  const updateLocalStorage = (newData) => {
+    localStorage.setItem("selectedArtists", JSON.stringify(newData));
+    window.dispatchEvent(new CustomEvent("localDataUpdated"));
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const storedArtists = JSON.parse(
+        localStorage.getItem("selectedArtists") || "[]"
+      );
+      // Only update if there are changes
+      if (JSON.stringify(storedArtists) !== JSON.stringify(items)) {
+        setItems(storedArtists);
+      }
+    }, 1000); // Every 1000 milliseconds
+
+    return () => clearInterval(intervalId);
+  }, [items]);
+
+  // below we remove an artist based on when the close button is pressed
+  const removeArtist = (index) => {
+    const newItems = items.filter((item, idx) => idx !== index);
+    setItems(newItems);
+    localStorage.setItem("selectedArtists", JSON.stringify(newItems));
+  };
+
   return (
     <div className="artistContainer">
-      <div className="container-fluid artist-card ">
+      <div className="container-fluid artist-card">
         <div className="row">
-          <div className="col-6">
-            <div className="close">
-              <CloseButton />
+          {items.map((artist, index) => (
+            <div className="col-6" key={artist.id}>
+              <div className="close" onClick={() => removeArtist(index)}>
+                <CloseButton />
+              </div>
+              <img
+                src={artist.images[0].url}
+                alt={artist.name}
+                className="artist-img"
+              />
+              <div className="artist-metaData">
+                <h6>
+                  Artist: <span className="artist-name">{artist.name}</span>
+                </h6>
+              </div>
             </div>
-            <img
-              src={Temp1}
-              alt="artist"
-              className="artist-img"
-              id="artistCover1"
-            />
-            <div className="artist-metaData">
-              <h6>
-                Artist{" "}
-                <span id="artistName1" className="artist-name">
-                  King Krule
-                </span>
-              </h6>
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="close">
-              <CloseButton />
-            </div>
-            <img
-              src={Temp1}
-              alt="artist"
-              className="artist-img"
-              id="artistCover2"
-            />
-            <div className="artist-metaData">
-              <h6>
-                Artist{" "}
-                <span id="artistName2" className="artist-name">
-                  King Krule
-                </span>
-              </h6>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <hr />
